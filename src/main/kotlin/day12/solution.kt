025bc9eaafe5b9dff41ff138355521f2aaf34694
Przemyslaw Materna <pmaterna@opera.com>
2021-12-12 11:16:13 +0100

    day 12

---
+++ src/main/kotlin/day12/solution.kt
@@ -0,0 +1,92 @@
+package day12
+
+import utils.isLowerCase
+import utils.isUpperCase
+
+val input = utils.input("day12/input.txt")
+    .readLines()
+    .map {
+        val n = it.split("-")
+        // map two-ways as it's a unidirectional graph
+        listOf(n[0] to n[1], n[1] to n[0]) //
+    }
+    .flatten()
+    .groupBy { it.first }
+    .mapValues { it.value.map { it.second } }
+
+fun main() {
+    println(getPaths(::selectSmallAtMostOnce).size)
+    println(getPaths(::selectSingleSmallTwice).size)
+}
+
+fun getPaths(selector: (List<String>) -> List<List<String>>): List<List<String>> {
+    val startingPaths = input.getOrElse("start") { throw IllegalArgumentException("no start entry") }
+        .map { listOf("start") + listOf(it) }
+
+    val finishedPaths = mutableListOf<List<String>>()
+    var currentPaths = startingPaths
+
+    while (currentPaths.isNotEmpty()) {
+        val partial = currentPaths.groupBy { it.last() == "end" }
+
+        val finished = partial.getOrDefault(true, listOf())
+        finishedPaths.addAll(finished)
+
+        currentPaths = partial.getOrDefault(false, listOf()).map {
+            selector(it)
+        }.flatten()
+    }
+
+    return finishedPaths
+}
+
+fun selectSingleSmallTwice(current: List<String>): List<List<String>> {
+    val thiz = input[current.last()]!!
+    val possible = thiz.map { current + it }
+
+    val filter = filter@{ v: List<String> ->
+        val groups = v.groupBy { it }
+        val lowerCaseCounters = groups
+            .filter { it.key.isLowerCase() && it.key != "start" && it.key != "end" }
+            .mapValues { it.value.size }
+
+        // verify max only one entry has more than one occurrence
+        if (lowerCaseCounters.filterValues { it > 1 }.size > 1) {
+            return@filter false
+        }
+
+        groups.all {
+            if (it.key.isUpperCase()) {
+                return@all true
+            } else if (it.key == "start" || it.key == "end") {
+                return@all it.value.size < 2 // cannot visit start/end twice
+            } else if (it.key.isLowerCase()) {
+                return@all it.value.size <= 2 // and verify it's up to 2 occurrences
+            } else {
+                throw IllegalArgumentException("only upper/lower case keys allowed")
+            }
+        }
+    }
+
+    return possible.filter(filter)
+}
+
+fun selectSmallAtMostOnce(current: List<String>): List<List<String>> {
+    val thiz = input[current.last()]!!
+    val possible = thiz.map { current + it }
+
+    val filter = { v: List<String> ->
+        v.groupBy { it }.all {
+            if (it.key.isUpperCase()) {
+                true
+            } else if (it.key.isLowerCase()) {
+                it.value.size < 2
+            } else {
+                throw IllegalArgumentException("only upper/lower case keys allowed")
+            }
+        }
+    }
+
+    // filter those which has two same entries
+    return possible.filter(filter)
+}
