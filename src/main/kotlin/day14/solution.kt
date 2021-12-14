1e9f277c9eb87f83ca2c079e2079b02cc8a13373
Przemyslaw Materna <pmaterna@opera.com>
2021-12-14 19:12:55 +0100

    day 14 - improvements

--- src/main/kotlin/day14/solution.kt
+++ src/main/kotlin/day14/solution.kt
@@ -3,21 +3,31 @@ package day14
 import utils.input
 import java.math.BigInteger
 
-val input = utils.input("day14/input.txt").readLines().first().map { it.toString() }
+val input = input("day14/input.txt")
+    .readLines()
+    .first()
+    .map { it }
 
-val rules = input("day14/input.txt").readLines().drop(2).map { it.split(" -> ") }
+val rules = input("day14/input.txt")
+    .readLines()
+    .drop(2)
+    .map { it.split(" -> ") }
     .associate { it.first() to it[0][0] + it[1] + it[0][1] }
 
 fun main() {
-    apply2(input, 10)
-    println(apply2(input, 40))
+    println(ex2(input, 10))
+    println(ex2(input, 40))
 }
 
-fun apply2(inn: List<String>, steps: Int): BigInteger {
-    val groups =
-        inn.zipWithNext().groupBy { it.first + it.second }.mapValues { it.value.count().toBigInteger() }.toMutableMap()
+fun ex2(inn: List<Char>, steps: Int): BigInteger {
+    val groups = inn.zipWithNext()
+        .groupBy { "${it.first}${it.second}" }
+        .mapValues { it.value.count().toBigInteger() }
+        .toMutableMap()
 
-    val letters = inn.groupBy { it }.mapValues { it.value.count().toBigInteger() }.toMutableMap()
+    val letters = inn.groupBy { it }
+        .mapValues { it.value.count().toBigInteger() }
+        .toMutableMap()
 
     // apply rules
     // for example, apply rule NN -> NCN
@@ -33,15 +43,15 @@ fun apply2(inn: List<String>, steps: Int): BigInteger {
             val replacement = rules[it.key]
 
             if (replacement != null) {
-                // values to repalace in string
+                // values to replace in string
                 val (p1, p2) = replacement.take(2) to replacement.takeLast(2)
                 // inserted letter
-                val l = replacement[1].toString()
+                val l = replacement[1]
 
-                groups[it.key] = groups[it.key]!! - it.value
-                groups.increaseOrAdd(p1, it.value)
-                groups.increaseOrAdd(p2, it.value)
-                letters.increaseOrAdd(l, it.value)
+                groups.putOrAdd(it.key, -it.value)
+                groups.putOrAdd(p1, it.value)
+                groups.putOrAdd(p2, it.value)
+                letters.putOrAdd(l, it.value)
             }
         }
     }
@@ -54,30 +64,30 @@ fun apply2(inn: List<String>, steps: Int): BigInteger {
     return max - min
 }
 
-fun <T> MutableMap<T, BigInteger>.increaseOrAdd(k: T, inc: BigInteger = BigInteger.ONE) {
-    if (this.contains(k)) {
-        this[k] = this[k]!! + inc
-    } else {
-        this[k] = inc
-    }
+fun <T> MutableMap<T, BigInteger>.putOrAdd(k: T, inc: BigInteger = BigInteger.ONE) {
+    this[k] = (this[k] ?: BigInteger.ZERO) + inc
 }
 
-fun ex1(steps: Int): Int {
+fun ex1(inn: String, steps: Int): Int {
     var current = input.joinToString("")
+
     repeat((0 until steps).count()) {
         current = replace(current)
     }
 
     val occ = current.groupBy { it }.mapValues { it.value.count() }
-    val (m, c) = occ.maxOf { it.value } to occ.minOf { it.value }
-    return m - c
+    val (max, min) = occ.maxOf { it.value } to occ.minOf { it.value }
+    return max - min
 }
 
 fun replace(inn: String): String {
-    val n = inn.zipWithNext().map { "${it.first}${it.second}" }.map {
-        if (rules.contains(it)) rules[it]!! else it
-    }
+    val n = inn.zipWithNext()
+        .map { "${it.first}${it.second}" }
+        .map { rules[it] ?: it }
 
     val includeLastChar = n.last().count() == 3 // if replaced attach the last character
-    return (n.map { it.take(2) } + if (includeLastChar) inn.last() else listOf<String>()).joinToString("")
+
+    return n.map { it.take(2) }
+        .plus(if (includeLastChar) inn.last() else listOf<String>())
+        .joinToString("")
 }
